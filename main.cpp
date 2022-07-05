9580b1acd62b249bac5e6bff3f6ac51fb030eca2
JohnCanessa <john.canessa@gmail.com>
2022-07-05 13:40:35 -0500

    initial main.cpp and operations.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief Experimenting with git stash
+ */
+int main ()
+{
+
+    // **** initialization ****
+    int retVal{0};
+
+
+
+
+    // **** clean up ****
+done:
+
+    // **** inform the caller what went on ****
+    return retVal;
+}
---
+++ operations.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief 
+ */
+int add (int a, int b)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ */
+int subtract(int a, int b)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ */
+int multiply(int a, int b)
+{
+    return 0;
+}
+
+/**
+ * @brief 
+ */
+int divide(int a, int b)
+{
+    return 0;
+}
